c92c9ce6dfd38d57c80983b22fc5108e1ae0f1ab
Aniswar Gupta <61372454+anish1357@users.noreply.github.com>
2026-04-10 00:29:26 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,32 @@
+class MinStack {
+    Stack<Integer> s1,s2;
+    public MinStack() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    
+    public void push(int val) {
+        s1.push(val);
+        if(s2.isEmpty() || (!s2.isEmpty() && val <= s2.peek())){
+            s2.push(val);
+        }
+    }
+    
+    public void pop() {
+        System.out.println("HI1");
+        if(!s2.isEmpty() && s2.peek().equals(s1.peek()) ){
+               System.out.println("HI2");
+                s2.pop();
+        }
+        s1.pop();
+        
+    }
+    
+    public int top() {
+        return s1.peek();
+    }
+    
+    public int getMin() {
+        return s2.peek();
+    }
+}
