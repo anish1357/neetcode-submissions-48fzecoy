01c38ef6ef0f4a90ca5efab9802f43534b5acb63
Aniswar Gupta <61372454+anish1357@users.noreply.github.com>
2026-04-15 03:05:35 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean isValid(int[] piles, int h, int m){
+        int ans = 0 ; 
+        for(int pile : piles ){
+            ans += pile % m == 0  ? pile/m : pile/m + 1;  
+        }
+        return ans <= h ;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int high = Arrays.stream(piles).max().getAsInt(); 
+        int low = 1; 
+        int ans = 0 ;
+        while(low < high ){
+            int mid = low + (high-low)/2;
+            if(isValid(piles,h,mid)){
+              ans = mid;
+              high = mid ;
+            }else {
+                low = mid +1 ; 
+            }
+        }
+      return low ;
+    }
+}
